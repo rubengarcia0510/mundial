4b5fa8e8e386b2934db590297f7532c994841aa8
Ruben Dario Garcia <rubendario0510@gmail.com>
2022-04-25 23:42:56 -0300

    test grupo.component

--- src/app/grupo/grupo.component.spec.ts
+++ src/app/grupo/grupo.component.spec.ts
@@ -5,6 +5,7 @@ import { GrupoComponent } from './grupo.component';
 describe('GrupoComponent', () => {
   let component: GrupoComponent;
   let fixture: ComponentFixture<GrupoComponent>;
+  let groupName:any
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,8 +15,24 @@ describe('GrupoComponent', () => {
   });
 
   beforeEach(() => {
+    groupName={
+      name:"ruben",
+      equipos:[
+        {
+          name:"boca",
+          pj:0,
+          pg:0,
+          pe:0,
+          pp:0,
+          gf:0,
+          gc:0,
+          puntos:0
+        }
+      ]
+    }
     fixture = TestBed.createComponent(GrupoComponent);
     component = fixture.componentInstance;
+    component.groupName=groupName;
     fixture.detectChanges();
   });
 
