d19aeb0b3a248328f22b6115d76789262cb4a35a
Ruben Garcia <rubengarcia0510@gmail.com>
2023-01-24 23:19:49 -0300

    handle login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'mundial';
   isLogued:boolean = false;
+
+  procesaPropagar(mensaje:boolean) {
+    this.isLogued=mensaje;
+    console.log(mensaje);
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  @Input()
+  logued!: boolean;
+
+  @Output() propagar = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onPropagar() {
+    console.log(this.logued)
+    this.logued=true;
+    this.propagar.emit(this.logued);
+  }
 
 }
